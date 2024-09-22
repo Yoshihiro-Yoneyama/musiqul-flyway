99c58fdcb82b0d553ea2793f9a0ee03ef6d0b8d4
yoshihiro <yoshihiro>
2024-09-22 15:21:18 +0900

    V9__create_table_query_recruitment.sqlの修正

--- migration/V9__create_table_query_recruitment.sql
+++ migration/V9__create_table_query_recruitment.sql
@@ -2,7 +2,7 @@ CREATE TABLE musiqul_query.recruitment
 (
     recruitment_id uuid primary key ,
     recruitment_name text,
-    owner_id uuid,
+    owner_id uuid UNIQUE ,
     song_title text,
     artist text,
     genre text[],
